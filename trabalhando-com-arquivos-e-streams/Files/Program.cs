8fe541c6322b73e1da59f5b3f4a01ff5df2606a1
Ronald Barbosa <ronaldverissimo@hotmail.com>
2022-03-03 10:49:15 -0300

    Trabalhando com arquivos e streams

---
+++ trabalhando-com-arquivos-e-streams/Files/Program.cs
@@ -0,0 +1,38 @@
+﻿// Criando arquivo de texto com nome definido pelo usuário
+// Caracteres inválidos no nome do arquivo serão trocados por '-'
+using System.IO;
+using static System.Console;
+
+namespace Files
+{
+   public class Program
+    {
+        public static void CreateFile(string path)
+        {
+            using var sw = File.CreateText(path);
+            sw.WriteLine("Linha 1");
+            sw.WriteLine("Linha 2");
+            sw.WriteLine("Linha 3");
+        
+        }
+        public static string ClearFileName(string name)
+        {
+            foreach(var @char in Path.GetInvalidFileNameChars())
+            {
+                name = name.Replace(@char, '-');
+            }
+            return name;
+        }
+        public static void Main(string[] args)
+        {
+            var name = ReadLine();
+            name = ClearFileName(name);
+
+            var path = Path.Combine("/tmp", $"{name}.txt");
+            CreateFile(path);
+        
+            WriteLine("Press 'Enter' to exit...");
+            ReadLine();
+        }
+    }  
+}
---
+++ trabalhando-com-arquivos-e-streams/Folders/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using static System.Console;
+
+namespace Folders 
+{
+    public class Program
+    {
+        public static void CreateFile(string name)
+        {
+            var path = Path.Combine("/tmp", $"{name}.txt");
+            if(!File.Exists(path))
+            {
+                using var sw = File.CreateText(path);
+            }
+            
+        }
+        public static void MoveFile(string originPath, string destinationPath)
+        {
+            if(!File.Exists(originPath))
+            {
+                WriteLine("File don't exists in origin path!"); 
+                return;
+            }
+            if(File.Exists(destinationPath)) 
+            {
+                WriteLine("File already exists in destination path!"); 
+                return;
+            }
+            
+            File.Move(originPath, destinationPath);
+        }
+        public static void CreateWorldDirectories()
+        {
+            var path = Path.Combine("/tmp", "world");
+
+            if(!Directory.Exists(path))
+            {
+                var world = Directory.CreateDirectory(path);
+            
+                var NorthAmerica = world.CreateSubdirectory("NorthAmerica");
+                var CentralAmerica = world.CreateSubdirectory("CentralAmerica");
+                var SouthAmerica = world.CreateSubdirectory("SouthAmerica");
+
+                NorthAmerica.CreateSubdirectory("USA");
+                NorthAmerica.CreateSubdirectory("MEX");
+
+                CentralAmerica.CreateSubdirectory("CUB");
+                CentralAmerica.CreateSubdirectory("PAN");
+
+                SouthAmerica.CreateSubdirectory("BRA");
+                SouthAmerica.CreateSubdirectory("ARG");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            CreateWorldDirectories();
+
+            CreateFile("Brasil");
+            
+            var origin = Path.Combine("/tmp", "Brasil.txt");
+            var destination = Path.Combine("/tmp", "world", "SouthAmerica", "BRA", "Brasil.txt");
+            
+            MoveFile(origin, destination);
+        }
+    }
+}
