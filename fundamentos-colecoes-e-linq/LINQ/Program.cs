a45e159321784ba81e29f61d0cc9769722dccba3
Ronald Barbosa <ronaldverissimo@hotmail.com>
2022-03-08 23:00:39 -0300

    Fundamentos de Coleções e LINQ com .NET: LINQ

---
+++ fundamentos-colecoes-e-linq/LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace LINQ
+{
+    public class LINQ
+    {
+        public static void Main(string[] args)
+        {
+            int[] arrayNumeros = new int[5] {1, 2, 3, 4, 5};
+            var numerosParesQuery =
+                    from num in arrayNumeros
+                    where num % 2 == 0
+                    orderby num
+                    select num;
+            
+            var numerosImparesMetodo = arrayNumeros.Where(x => x % 2 ==1).OrderBy(x => x).ToList();
+
+            System.Console.WriteLine("Números pares query: " + string.Join(", ", numerosParesQuery));
+            System.Console.WriteLine("Números ímpares método: " + string.Join(", ", numerosImparesMetodo));
+        }
+    }
+}
